5596e33243d29a30b21babea86ea647cf8b1c35b
DanLowo <danlowo@gmail.com>
2022-08-23 04:21:25 +0100

    changes

--- js/index.js
+++ js/index.js
@@ -2,6 +2,32 @@ const mobile_client_img =
   "https://www.econsor.de/wp-content/uploads/2021/09/unsere-kunden-2.jpg.webp";
 const desktop_client_img =
   "https://www.econsor.de/wp-content/uploads/2021/09/unsere-kunden-4.jpg.webp";
+const mobile_certification =
+  "https://www.econsor.de/wp-content/uploads/2022/06/econsor-partner-mobil.jpg.webp";
+
+const locationDetails = {
+  "Digital Agency Stuttgart":
+    "We are your digital agency in Stuttgart for individual requirements and solutions for your website. Get to know us",
+  "Digital Agency Heilbronn":
+    "Tailor-made strategies and individual solutions from our experts in Heilbronn. Your digital agency in the center.",
+  "Digital Agency Heidelberg":
+    "Our digital agency in Heidelberg offers you all services related to online shops, websites, web design and online marketing.",
+  "Digital Agency Nuremberg":
+    "Are you looking for a digital agency in the Nuremberg area? Our experts will be happy to advise you in Nuremberg and the surrounding area.",
+  "Digital Agency Munich":
+    "You will find our digital agency in the center of Munich. Come by for a personal consultation.",
+  "Digital Agency Frankfurt":
+    "Our experts are also available in Frankfurt. We offer you sustainable solutions for your online presence.",
+};
+
+window.addEventListener("scroll", function () {
+  const nav2 = document.querySelector(".nav-2");
+  if (this.scrollY > 40) {
+    nav2.style.position = "fixed";
+  } else {
+    nav2.style.position = "sticky";
+  }
+});
 
 function isMobileDevice() {
   const screenSize = window.innerWidth;
@@ -9,9 +35,8 @@ function isMobileDevice() {
   if (screenSize < 700) {
     return true;
   }
-  return false
+  return false;
 }
-
 
 function cardHover(element) {
   element.classList.add("active");
@@ -30,26 +55,42 @@ function workCardHoverLeave(element) {
 }
 
 function handleNavigationBar() {
-  const navigation = document.querySelector(".nav-drawer")
-  
-  if(navigation.classList.contains("open")) {
-    document.body.style.overflow = "auto"
-    navigation.style.display = "none"
-    navigation.classList.remove("open")
+  const navigation = document.querySelector(".nav-drawer");
 
-    return
+  if (navigation.classList.contains("open")) {
+    document.body.style.overflow = "auto";
+    navigation.style.display = "none";
+    navigation.classList.remove("open");
+
+    return;
   }
-  document.body.style.overflow = "clip"
-  navigation.style.display = "flex"
-  navigation.classList.add("open")
+  document.body.style.overflow = "clip";
+  navigation.style.display = "flex";
+  navigation.classList.add("open");
 }
 
 function handleHoverDropDown(element) {
-  const div = element.firstElementChild.nextElementSibling
-  div.style.display = "flex"
+  const text = element.firstElementChild;
+  text.style.color = "var(--light-blue)";
+
+  const div = text.nextElementSibling;
+  div.style.display = "inline-block";
 }
 
 function handleLeaveDropDown(element) {
-  const div = element.firstElementChild.nextElementSibling
-  div.style.display = "none"
+  const text = element.firstElementChild;
+  text.style.color = "black";
+
+  const div = text.nextElementSibling;
+  div.style.display = "none";
+}
+
+function handleLocation(element) {
+  const title = element.firstElementChild.textContent.trim();
+  const paragraph = document.querySelector("#about-location");
+  paragraph.textContent = locationDetails[title];
+
+  paragraph.parentElement.lastElementChild.firstElementChild.textContent = `Go To ${
+    title.split(" ")[1]
+  } ${title.split(" ")[2]}`;
 }
--- js/onload.js
+++ js/onload.js
@@ -6,3 +6,27 @@
     clientImgElement.src = mobile_client_img;
   }
 })();
+
+(function loadCertificationImage() {
+  const clientImgElement = document.querySelector("#certification-img");
+  const size = isMobileDevice();
+
+  if (size) {
+    clientImgElement.src = mobile_certification;
+  }
+})();
+
+(function addScrollsToRefrences() {
+  if (isMobileDevice()) {
+    const locationDetails = document.querySelector(".location .details");
+    const allLocations = document.querySelectorAll(
+      ".location .items .story-card"
+    );
+
+    for (locationItem of allLocations) {
+      locationItem.addEventListener("click", () => {
+        locationDetails.scrollIntoView({ behavior: "smooth" });
+      });
+    }
+  }
+})();
